8ca5d81848baa58290e4aa578cb1d35ad089292c
Stefan Simonovic <stefan@simonovic.at>
2013-09-26 11:51:33 +0200

    Konflikt local

--- Spengergasse_POS_13-14/src/Hallo.java
+++ Spengergasse_POS_13-14/src/Hallo.java
@@ -3,7 +3,7 @@ public class Hallo {
 	public static void main(String[] args) {
 		System.out.println("Hallo");
 		System.out.println("Hallo from Git");
-		System.out.println("Halll2");
+		System.out.println("Halllo");
 
 	}
 
